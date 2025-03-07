50983ff0763d84e1d7acdcac7de4d5bc185c215a
Jash Mody <modyjash@gmail.com>
2025-03-07 18:20:49 -0500

    Footer and the Final Website Created

--- script.js
+++ script.js
@@ -37,16 +37,24 @@ function loading() {
 
 loading();
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
-
-var elems = document.querySelectorAll(".elem");
-var page2 = document.querySelector("#page2");
-elems.forEach(function (ele) {
-  ele.addEventListener("mouseenter", function () {
-    var bgimg = ele.getAttribute("data-img");
-    page2.style.backgroundImage = `url(${bgimg})`;
+function loco() {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
   });
-});
+
+  document.querySelector("#footer h2").addEventListener("click", () => {
+    scroll.scrollTo(0);
+  });
+
+  var elems = document.querySelectorAll(".elem");
+  var page2 = document.querySelector("#page2");
+  elems.forEach(function (ele) {
+    ele.addEventListener("mouseenter", function () {
+      var bgimg = ele.getAttribute("data-img");
+      page2.style.backgroundImage = `url(${bgimg})`;
+    });
+  });
+}
+
+loco();
